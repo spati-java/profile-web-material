af3c08d57fe5321637e8ecba50b0ce9ffa21ffdc
Sourav Pati <sourav.pati09@gmail.com>
2017-09-04 16:44:14 -0500

    more changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from '@angular/material';
 import {MdToolbarModule} from '@angular/material';
 import 'hammerjs';
+import { ExpansionPanelsModule } from 'ng2-expansion-panels';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import {MdButtonModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ProjectComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    MdToolbarModule
+    MdToolbarModule,
+    ExpansionPanelsModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
